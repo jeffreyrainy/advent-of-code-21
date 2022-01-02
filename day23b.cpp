b400f541f9a908aa39ebf63c5e45bd50338f108d
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2022-01-02 00:53:01 -0500

    proper approach for day 23

--- day23b.cpp
+++ day23b.cpp
@@ -27,16 +27,20 @@ public:
     char inter[3] = {'.', '.', '.'}; // spots between columns
     char l[2] = {'.', '.'}; // two leftmost spots, [0] most accessible
     char r[2] = {'.', '.'}; // two rightmost spots, [0] most accessible
+    static constexpr const long factor[4] = {1, 10, 100, 1000};
 
     long cost = 0;
 
-    bool Done() const {return false;}
+    bool Done() const;
     vector<State<Depth>> GetNext() const;
-    void Best() const {};
+    void Best() const { cout << *this << endl; };
     bool Accept() const {return true;}
 
     bool ColReady(long col) const;
     void Simplify();
+    long costColInter(long col, long inter, long id) const;
+    long costColCol(long colTo, long colFrom) const;
+
 };
 
 template<long Depth>
@@ -88,7 +92,58 @@ bool State<Depth>::ColReady(long col) const
     return true;
 }
 
-//todo: bump L0 to L1 and R0 to R1
+template<long Depth>
+bool State<Depth>::Done() const
+{
+    for(long col = 0; col < 4; col++)
+    {
+        if (cols[col][0] != 'A' + col)
+        {
+            return false;
+        }
+    }
+    for(long col = 0; col < 4; col++)
+    {
+        if (!ColReady(col))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<long Depth>
+long State<Depth>::costColInter(long col, long inter, long id) const
+{
+    long ret = 0;
+
+    if (inter >= col)
+    {
+        ret = ((inter - col) * 2 + 1 + // side moves
+                tops[col]) // get in cost
+                * factor[id];
+    }
+    else
+    {
+        ret = ((col - inter) * 2 - 1 + // side moves
+                tops[col]) // get in cost
+                * factor[id];
+    }
+
+    return ret;
+}
+
+template<long Depth>
+long State<Depth>::costColCol(long colTo, long colFrom) const
+{
+    long ret = (std::abs(colTo - colFrom) * 2 + // side moves
+            tops[colTo] + // get in cost
+            tops[colFrom] + 1) // get out cost
+            * factor[colTo];
+
+    return ret;
+}
+
 
 template<long Depth>
 void State<Depth>::Simplify()
@@ -107,6 +162,9 @@ void State<Depth>::Simplify()
                 {
                     if (inter[si] == 'A' + col)
                     {
+                        long c1 = costColInter(col, si, col);
+                        cost += c1;
+
                         cols[col][--tops[col]] = 'A' + col;
                         inter[si] = '.';
                         changed = true;
@@ -117,6 +175,9 @@ void State<Depth>::Simplify()
                     }
                     if (tops[si + 1] != Depth && cols[si + 1][tops[si + 1]] == 'A' + col)
                     {
+                        long c1 = costColCol(col, si + 1);
+                        cost += c1;
+
                         cols[col][--tops[col]] = 'A' + col;
                         ++tops[si + 1];
                         changed = true;
@@ -126,8 +187,20 @@ void State<Depth>::Simplify()
                 {
                     if (r[0] == 'A' + col)
                     {
+                        long c1 = costColInter(col, 3, col);
+                        cost += c1;
+
                         cols[col][--tops[col]] = 'A' + col;
                         r[0] = '.';
+                        changed = true;
+                    }
+                    else if (r[1] == 'A' + col && r[0] == '.')
+                    {
+                        long c1 = costColInter(col, 3, col) + factor[col];
+                        cost += c1;
+
+                        cols[col][--tops[col]] = 'A' + col;
+                        r[1] = '.';
                         changed = true;
                     }
                 }
@@ -137,6 +210,9 @@ void State<Depth>::Simplify()
                 {
                     if (inter[si] == 'A' + col)
                     {
+                        long c1 = costColInter(col, si, col);
+                        cost += c1;
+
                         cols[col][--tops[col]] = 'A' + col;
                         inter[si] = '.';
                         changed = true;
@@ -147,6 +223,9 @@ void State<Depth>::Simplify()
                     }
                     if (tops[si] != Depth && cols[si][tops[si]] == 'A' + col)
                     {
+                        long c1 = costColCol(col, si);
+                        cost += c1;
+
                         cols[col][--tops[col]] = 'A' + col;
                         ++tops[si];
                         changed = true;
@@ -156,8 +235,20 @@ void State<Depth>::Simplify()
                 {
                     if (l[0] == 'A' + col)
                     {
+                        long c1 = costColInter(col, -1, col);
+                        cost += c1;
+
                         cols[col][--tops[col]] = 'A' + col;
                         l[0] = '.';
+                        changed = true;
+                    }
+                    else if (l[1] == 'A' + col && l[0] == '.')
+                    {
+                        long c1 = costColInter(col, -1, col) + factor[col];
+                        cost += c1;
+
+                        cols[col][--tops[col]] = 'A' + col;
+                        l[1] = '.';
                         changed = true;
                     }
                 }
@@ -165,11 +256,7 @@ void State<Depth>::Simplify()
         }
     }
     while(changed);
-
-
-
 }
-
 
 template<long Depth>
 vector<State<Depth>> State<Depth>::GetNext() const
@@ -192,6 +279,7 @@ vector<State<Depth>> State<Depth>::GetNext() const
                 target = *this;
                 target.inter[si] = target.cols[col][target.tops[col]];
                 target.tops[col]++;
+                target.cost += target.costColInter(col, si, target.inter[si] - 'A');
 
                 target.Simplify();
 
@@ -199,11 +287,18 @@ vector<State<Depth>> State<Depth>::GetNext() const
             }
             if (si == 3) // stack to the right
             {
-                if (r[0] == '.')
+                target = *this;
+                if (r[1] == '.' && r[0] != '.')
                 {
-                    target = *this;
+                    target.r[1] = r[0];
+                    target.r[0] = '.';
+                    target.cost += factor[target.r[1] - 'A'];
+                }
+                if (target.r[0] == '.')
+                {
                     target.r[0] = target.cols[col][target.tops[col]];
                     target.tops[col]++;
+                    target.cost += target.costColInter(col, 3, target.r[0] - 'A');
 
                     target.Simplify();
 
@@ -222,6 +317,7 @@ vector<State<Depth>> State<Depth>::GetNext() const
                 target = *this;
                 target.inter[si] = target.cols[col][target.tops[col]];
                 target.tops[col]++;
+                target.cost += target.costColInter(col, si, target.inter[si] - 'A');
 
                 target.Simplify();
 
@@ -229,11 +325,18 @@ vector<State<Depth>> State<Depth>::GetNext() const
             }
             if (si == -1) // stack to the left
             {
-                if (l[0] == '.')
+                target = *this;
+                if (l[1] == '.' && l[0] != '.')
                 {
-                    target = *this;
+                    target.l[1] = l[0];
+                    target.l[0] = '.';
+                    target.cost += factor[target.l[1] - 'A'];
+                }
+                if (target.l[0] == '.')
+                {
                     target.l[0] = target.cols[col][target.tops[col]];
                     target.tops[col]++;
+                    target.cost += target.costColInter(col, -1, target.l[0] - 'A');
 
                     target.Simplify();
 
@@ -244,50 +347,13 @@ vector<State<Depth>> State<Depth>::GetNext() const
         }
     }
 
-    cout << "=======" << endl;
-    cout << *this << endl;
-
-    for(const auto& s:ret)
-    {
-        cout << s << endl;
-    }
-
-    cout << endl;
-
-
     return ret;
 }
 
 void process(Data& data)
 {
     State<2> start;
-
-/*
-    start.cols[0][0] = 'X';
-    start.cols[0][1] = 'X';
-
-    start.cols[1][0] = 'D';
-    start.cols[1][1] = 'D';
-
-    start.cols[2][0] = 'A';
-    start.cols[2][1] = 'B';
-
-    start.cols[3][0] = 'C';
-    start.cols[3][1] = 'B';
-
-    start.inter[0] = 'A';
-    start.l[0] = 'C';
-
-    start.tops[0] = 2;
-
-    cout << start << endl;
-
-    cout << start.ColReady(0) << endl;
-    start.Simplify();
-
-    cout << start << endl;
-*/
-
+    State<4> start4;
 
     for(long i = 0; i < 4; i++)
     {
@@ -295,9 +361,27 @@ void process(Data& data)
         start.cols[i][1] = data.words[3][0][i];
     }
 
-    cout << start << endl;
-
     Explorer<State<2>> e;
     e.Explore(start);
+
+
+    for(long i = 0; i < 4; i++)
+    {
+        start4.cols[i][0] = data.words[2][0][i];
+        start4.cols[i][3] = data.words[3][0][i];
+    }
+
+    start4.cols[0][1] = 'D';
+    start4.cols[1][1] = 'C';
+    start4.cols[2][1] = 'B';
+    start4.cols[3][1] = 'A';
+
+    start4.cols[0][2] = 'D';
+    start4.cols[1][2] = 'B';
+    start4.cols[2][2] = 'A';
+    start4.cols[3][2] = 'C';
+
+    Explorer<State<4>> e4;
+    e4.Explore(start4);
 
 }
