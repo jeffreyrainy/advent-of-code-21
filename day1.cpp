e4f223ddbae535d8a029a6218fd0af9bddfd6b83
Jeffrey Rainy <jeffreyrainy@gmail.com>
2024-12-01 11:18:35 -0500

    day 1, 2024

--- day1.cpp
+++ day1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "data.h"
 
+using namespace std;
+
 std::map<char, std::string> getReplacements()
 {
     std::map<char, std::string> ret{};
@@ -9,23 +11,34 @@ std::map<char, std::string> getReplacements()
 
 void process(Data& data)
 {
-    int count = 0;
-    for(int i = 1; i < data.numbers.size(); i++)
-    {
-        if (data.numbers[i][0] > data.numbers[i - 1][0])
-        {
-            count ++;
-        }
-    }
-    std::cout << count << std::endl;
+    vector<int> l1, l2;
+    map<int, int> counts1, counts2;
 
-    count = 0;
-    for(int i = 3; i < data.numbers.size(); i++)
+    for(int i = 0; i < data.numbers.size(); i++)
     {
-        if (data.numbers[i][0] > data.numbers[i - 3][0])
-        {
-            count ++;
-        }
+        int i1 = data.numbers[i][0];
+        int i2 = data.numbers[i][1];
+
+        l1.push_back(i1);
+        l2.push_back(i2);
+
+        counts1[i1]++;
+        counts2[i2]++;
     }
-    std::cout << count << std::endl;
+
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
+    int sum = 0;
+    int simil = 0;
+    for(int i = 0; i < l1.size();  i++)
+    {
+        sum += abs(l1[i] - l2[i]);
+
+        simil += l1[i] * counts2[l1[i]];
+    }
+    cout << sum << endl;
+    cout << simil << endl;
+
+
+
 }
